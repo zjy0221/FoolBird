be232d80698ddc8d3687c6f78c71a9dfe5089cbe
zjy0221 <s9611918@gmail.com>
2023-06-02 15:43:31 +0800

    我再努力了

--- Assets/主程式素材/遊戲控制程式/Game_Manager.cs
+++ Assets/主程式素材/遊戲控制程式/Game_Manager.cs
@@ -8,15 +8,24 @@ namespace Platformer
     public class Game_Manager : MonoBehaviour
     {
         public int coinsCounter = 0;
+        public int coinSum = 0;
 
         public GameObject playerGameObject;
         private PlayerCode player;
         public GameObject deathPlayerPrefab;
         public Text coinText;
 
+        public GameObject result;
+        public Text coinResult_Text;
+        public Text timeResult_Text;
+        float time_f;
+        int time_i;
+
         void Start()
         {
             player = GameObject.Find("Player").GetComponent<PlayerCode>();
+            result.SetActive(false);
+            
         }
 
         void Update()
@@ -30,11 +39,33 @@ namespace Platformer
                 player.deathState = false;
                 Invoke("ReloadLevel", 3);
             }
+
+            Result_State();
         }
 
         private void ReloadLevel()
         {
             Application.LoadLevel(Application.loadedLevel);
         }
+
+        void Result_State()
+        {
+            if (result.active == true)
+            {
+                coinSum = coinsCounter;
+                coinResult_Text.text = coinSum.ToString();
+
+                time_f = Time.time;
+                time_i = Mathf.FloorToInt(time_f);
+                Time.timeScale = 0;
+                timeResult_Text.text = time_i.ToString();
+            }
+            else 
+            {
+                Time.timeScale = 1;
+            }
+        }
+
+
     }
 }
---
+++ Assets/主程式素材/遊戲控制程式/btn_click.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btn_click : MonoBehaviour
+{
+    public void OnMouseDown() 
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
